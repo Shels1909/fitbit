d3c32aab4e978f9eb13723e5bf6b5207a4ecd8f2
Mac Knight <mknight1996@gmail.com>
2017-11-24 22:10:23 -0500

    Heart Rate Montitor reaads data from an external file and updates current heart rate. The HeartRateMontor is running in its own thread

---
+++ src/fitbit/HeartRateMonitor.java
@@ -0,0 +1,60 @@
+package fitbit;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class HeartRateMonitor extends Thread {
+	private float currentHeartRate;
+	
+	public HeartRateMonitor() {
+		// set the initial heart rate to 0
+		currentHeartRate = 0;
+	}
+	@Override 
+	public void run() {
+		startMonitoringPulse();
+	}
+	public void startMonitoringPulse() {			
+			try {
+				readPulseDataFromFile("pulse_data.txt");
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+	}
+	
+	public float getCurrentHeartRate() {
+		return currentHeartRate;
+	}
+	
+	public void readPulseDataFromFile(String path) throws InterruptedException{
+    		BufferedReader pulseReader = null;
+    		
+    		try{   			
+    			pulseReader = new BufferedReader(new FileReader(path));
+    			String pulse;
+    			while((pulse = pulseReader.readLine()) != null){
+    				currentHeartRate = Float.parseFloat(pulse);
+    				Thread.sleep(10000);
+    			}
+    		}
+    		catch(IOException e){
+    			e.printStackTrace();
+    		}
+    		finally {
+    			if(pulseReader != null) {
+    				try {
+					pulseReader.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+    			}
+    		}
+    		        		
+    }
+	
+}
